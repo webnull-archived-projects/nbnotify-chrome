a6c7c2c9d9b72a4a53ef27fdf61ed916ac98b833
webnull <webnull.www@gmail.com>
2012-12-26 16:20:17 +0100

    Added facebook integration

--- js/options.js
+++ js/options.js
@@ -1,8 +1,64 @@
 var i18n=function(){function i(b){b=b.querySelectorAll(l);for(var d,f=0;d=b[f];f++)for(var e=0;e<h.length;e++){var c=h[e],a=d.getAttribute(c);a!=null&&j[c](d,a)}}var j={"i18n-content":function(b,d){b.textContent=chrome.i18n.getMessage(d)},"i18n-values":function(b,d){for(var f=d.replace(/\s/g,"").split(/;/),e=0;e<f.length;e++){var c=f[e].match(/^([^:]+):(.+)$/);if(c){var a=c[1];c=chrome.i18n.getMessage(c[2]);if(a.charAt(0)=="."){a=a.slice(1).split(".");for(var g=b;g&&a.length>1;)g=g[a.shift()];if(g){g[a]=c;a=="innerHTML"&&i(b)}}else b.setAttribute(a,c)}}}},h=[],k;for(k in j)h.push(k);var l="["+h.join("],[")+"]";return{process:i}}();
 
+function checkFacebookIntegration() {
+    // check if any facebook feed link exists in database
+    chrome.extension.sendMessage({type: "memory"}, function(response) {
+
+        d = response.data;
+        for (i in d) {
+            if(d[i].indexOf("facebook.com/feeds/notifications.php") != -1)
+            {
+                console.log("Found facebook link: "+d[i]);
+                $('#facebook_integration_div').remove();
+                return false;
+            }
+        }
+    });
+}
+
+
+function facebookIntegration() {
+    // add facebook link to database
+
+    $.ajax({type: "GET",
+            url: 'https://www.facebook.com/notifications',
+            headers: { 
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Referer": "https://www.facebook.com/",
+                "Cache-Control": "max-age=0"
+            },
+            async: false,
+            success: function(response) {
+                parser = new DOMParser();
+                xmlDoc = parser.parseFromString(response,"text/xml");
+                link = false;    
+
+                var r = new RegExp('viewer=([0-9]+)\&amp\;key=([A-Za-z0-9]+)\&amp\;format');
+                test = r.exec(response);
+
+                if (test.length > 0)
+                {
+                    // 1 => id & viewer, 2 => key
+                    facebookLink = "https://www.facebook.com/feeds/notifications.php?id="+test[1]+"&viewer="+test[1]+"&key="+test[2]+"&format=rss20";
+                    chrome.extension.sendMessage({type: "addPage", link: facebookLink}, function(response) { })
+                    $('#facebook_integration_div').remove();
+                    refreshList();
+                } else {
+                    alert(chrome.i18n.getMessage("cannot_connect_facebook"));                
+                }
+            }
+           });
+}
+
 function removePage(e) {
     adress = e.target.rel;
     chrome.extension.sendMessage({type: "removePage", link: adress}, function(response) { listAllPlugins() })
+    refreshList();
+}
+
+function refreshList() {
+    listAllPlugins();
+    i18n.process(document);
 }
 
 function listAllPlugins() {
@@ -43,8 +99,7 @@ function listAllPlugins() {
 }
 
 function init() {
-    listAllPlugins();
-    i18n.process(document);
+    refreshList();
 }
 
 function checkHost(host) {
@@ -74,6 +129,10 @@ function checkHost(host) {
 }
 
 $(function(){
+    // facebook integration
+    checkFacebookIntegration();
+    document.getElementById('facebook_integration').addEventListener("click", facebookIntegration, false);
+
     // global -> checktime
     $('#checktime').val((localStorage.checktime/60));
     $('#checktime').focusout(function () { 
